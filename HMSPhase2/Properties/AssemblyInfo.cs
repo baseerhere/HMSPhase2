f084f759057caa15a8202a0b262c3ffcc4c83434
Baseer Siddiqui <baseerhere@gmail.com>
2017-12-23 19:57:48 +0530

    Updating Dev Environment

--- HMSPhase2/Properties/AssemblyInfo.cs
+++ HMSPhase2/Properties/AssemblyInfo.cs
@@ -9,7 +9,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HMSPhase2")]
+[assembly: AssemblyProduct("Hotel Management System")]
 [assembly: AssemblyCopyright("Copyright © 2017 ")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]

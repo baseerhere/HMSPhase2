ccababc69c97d86a75cdde5f3652b3388a01f588
Baseer Siddiqui <baseerhere@gmail.com>
2018-01-11 05:28:20 +0530

    Azure functions

---
+++ HMSPhase2.DBModels/Restaurant.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HMSPhase2.DBModels
+{
+    class Restaurant
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ShortCode { get; set; }
+        public User Administrator { get; set; }
+        public bool IsActive { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime CreatedBy { get; set; }
+    }
+}
---
+++ HMSPhase2.DBModels/User.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace HMSPhase2.DBModels
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public bool IsActive { get; set; }
+        
+    }
+}
